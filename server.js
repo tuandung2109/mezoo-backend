ffd33dae7d7f78a7913a34df130bed7b7ffdc375
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-12-01 12:06:59 +0700

    sửa lỗi quên mật khẩu

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins for now to fix CORS issue
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '50mb' })); // Increase limit for base64 images
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
