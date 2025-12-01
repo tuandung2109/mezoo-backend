e0629aaf1bca1835b43ce9d5549eea84357df706
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-12-01 11:15:38 +0700

    Sửa code gửi mail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,15 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
+  // Create transporter
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    service: 'gmail', // Use built-in service for Gmail
     auth: {
       user: process.env.SMTP_EMAIL,
-      pass: process.env.SMTP_PASSWORD
+      pass: process.env.SMTP_PASSWORD ? process.env.SMTP_PASSWORD.replace(/\s+/g, '') : ''
     }
   });
 
+  // Define email options
   const message = {
     from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
     to: options.email,
@@ -17,6 +18,7 @@ const sendEmail = async (options) => {
     text: options.message
   };
 
+  // Send email
   const info = await transporter.sendMail(message);
 
   console.log('Message sent: %s', info.messageId);
